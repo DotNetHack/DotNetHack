69b693785dd7e6c4a81e8bc6a4062e0de4d6c516
Pete Jensen <jensen.petej@gmail.com>
2011-06-24 23:53:49 -0700

    Added Affects, AffectResistance, AffectType(s) and Modifiers.  "Actor" is the class upon which affects act upon.  Both players and NPC's are considered an *actor* (for now).  This affect implementation is somewhat experimental as noted in GameEngine.cs

---
+++ src/DotNetHack/Game/Actor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotNetHack.Game.Affects;
+
+namespace DotNetHack.Game
+{
+    /// <summary>
+    /// Actor
+    /// </summary>
+    public class Actor
+    {
+        public Actor()
+
+        { Health = 100; }
+        public int Health { get; set; }
+
+        public void ApplyAffects()
+        {
+            foreach (Affect affect in AffectStack)
+                affect.ApplyTo(this);
+        }
+
+        public Stack<AffectResistance> ResistanceStack = new Stack<AffectResistance>();
+        public Stack<Affect> AffectStack = new Stack<Affect>();
+    }
+}
---
+++ src/DotNetHack/Game/Affects/AffectModifiers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetHack.Game.Affects
+{
+    /// <summary>
+    /// AffectModifiers
+    /// </summary>
+    public static class AffectModifiers
+    {
+        /// <summary>
+        /// Damages target by base + 7% of remaining health
+        /// Disease restistance applied.
+        /// </summary>
+        /// <param name="affect"></param>
+        /// <param name="target"></param>
+        public static void Pestilence(Affect affect, Actor target)
+        {
+            Affect tmpAffect = new Affect(affect.AffectType, affect.Magnitude, affect.Duration);
+            foreach (var resistance in target.ResistanceStack)
+                tmpAffect = resistance.Apply(tmpAffect);
+            tmpAffect.Magnitude +=
+                ((double)(target.Health * 0.07));
+            target.Health -= (int)tmpAffect.Magnitude;
+        }
+    }
+}

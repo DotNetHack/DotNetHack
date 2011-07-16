374ff9fa5b5e3479cd102064613b26a4a1f96319
Alex Hart <alex.hart658@gmail.com>
2011-07-16 15:13:40 -0400

    Add conversion method for displaying currency.
    This method can be used in menues or anywhere else the amount
    of currency the unit poseses is displayed.  This will take the
    amount stored in the object instance and break it into gold,
    silver, and copper.  This should resolve the first bit of issue
    20.  The operator will be added shortly.

--- src/DotNetHack/Game/Items/Currency.cs
+++ src/DotNetHack/Game/Items/Currency.cs
@@ -14,6 +14,25 @@ namespace DotNetHack.Game.Items
             : base("gold", '$', Colour.Yellow)
         { Amount = aAmt; }
 
+        // Store the amount of currency in copper
         public int Amount { get; set; }
+
+        /// <summary>
+        /// DisplayAmount
+        /// Gets a three element array for currency display to
+        /// user.
+        /// </summary>
+        public int[] DisplayAmount()
+        {
+            // Take our amount and convert it into Gold/Silver/Copper
+            // 1 Gold = 100 Silver, 1 Silver = 100 Copper
+            // 1 Gold = 10000 Copper
+            int [] amounts = new int [3];
+            amounts[0] = Amount / 10000;
+            amounts[1] = (Amount % 10000 - Amount % 100) / 100;
+            amounts[2] = Amount % 100;
+
+            return amounts;
+        }
     }
 }

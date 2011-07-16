d3e59d15a14ce0a9b58ee6bdcf610c4746bf8394
Pete Jensen <jensen.petej@gmail.com>
2011-07-16 09:40:49 -0700

    Added EditorMode { Layout, Items, Monsters }

---
+++ src/DotNetHack.Editor/EditorMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetHack.Editor
+{
+    /// <summary>
+    /// The various editor modes that can be switched between.
+    /// </summary>
+    public enum EditorMode
+    {
+        Layout,
+        Items,
+        Monsters,
+    }
+}

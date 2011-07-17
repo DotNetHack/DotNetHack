26f9d1ae5f941458ba3e9ea41ef02e4daf8bee15
Pete Jensen <jensen.petej@gmail.com>
2011-07-17 14:28:19 -0700

    IKey and IHasLock interfaces have been added.

---
+++ src/DotNetHack/Game/Interfaces/IKey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetHack.Game.Interfaces
+{
+    public interface IKey
+    {
+        public Guid KeyGuid { get; set; }
+    }
+
+    public interface IHasLock
+    {
+        public void UnLock(IKey aKey);
+        public IKey KeyGuid { get; set; }
+    }
+}

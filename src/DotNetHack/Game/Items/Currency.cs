c949b31813bab041d0d9f3e8671fe2bee8bed0b1
Pete Jensen <jensen.petej@gmail.com>
2011-07-16 17:36:43 -0700

    Updated Currency, left out Gold, Silver, Copper for now in favor of the quicker "amount" as in amount of gold coins or "zorkmids" in Nethack.

--- src/DotNetHack/Game/Items/Currency.cs
+++ src/DotNetHack/Game/Items/Currency.cs
@@ -8,12 +8,89 @@ namespace DotNetHack.Game.Items
     /// <summary>
     /// Currency
     /// </summary>
-    public class Currency : Item
+    public class Currency : Item, IEquatable<Currency>
     {
-        public Currency(int aAmt)
-            : base("gold", '$', Colour.Yellow)
-        { Amount = aAmt; }
+        /// <summary>
+        /// Currency
+        /// </summary>
+        /// <param name="aAmount">Amount</param>
+        public Currency(int aAmount)
+            : base("Gold", '$', Colour.Yellow) { }
 
+        /// <summary>
+        /// The amont of currency this represents.
+        /// </summary>
         public int Amount { get; set; }
+
+        /// <summary>
+        ///  operator +
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns>The new amount</returns>
+        static Currency operator +(Currency a, Currency b)
+        {
+            return new Currency(a.Amount + b.Amount);
+        }
+
+        /// <summary>
+        /// operator <
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static bool operator <(Currency a, Currency b)
+        {
+            return (a.Amount < b.Amount);
+        }
+
+        /// <summary>
+        /// operator >
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static bool operator >(Currency a, Currency b)
+        {
+            return (a.Amount > b.Amount);
+        }
+
+        /// <summary>
+        /// operator >=
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns>true when condition met</returns>
+        static bool operator >=(Currency a, Currency b) 
+        {
+            return (a.Amount >= b.Amount);
+        }
+
+        /// <summary>
+        /// operator <=
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns>true when condition met</returns>
+        static bool operator <=(Currency a, Currency b)
+        {
+            return (a.Amount <= b.Amount);
+        }
+
+        /// <summary>
+        /// Equals
+        /// </summary>
+        /// <param name="other">The Currency to compare to</param>
+        /// <returns><value>true</value> when equal</returns>
+        public bool Equals(Currency other)
+        {
+            return this.Amount.Equals(other.Amount);
+        }
+
+        /// <summary>
+        /// GetHashCode
+        /// </summary>
+        /// <returns>The amount is good enough for the hash</returns>
+        public override int GetHashCode() { return this.Amount; }
     }
 }

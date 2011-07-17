c9c226e36b863dffb82863378ffcd147aff40ca9
Pete Jensen <jensen.petej@gmail.com>
2011-07-17 14:28:41 -0700

    KeyChain, Stores keys.

---
+++ src/DotNetHack/Game/KeyChain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotNetHack.Game.Interfaces;
+
+namespace DotNetHack.Game
+{
+    /// <summary>
+    /// KeyChain
+    /// <remarks>Stores all keys that the player has come across.</remarks>
+    /// </summary>
+    public class KeyChain
+    {
+        /// <summary>
+        /// Creates a new instance of keychain.
+        /// </summary>
+        public KeyChain() { KeyStore = new List<IKey>(); }
+
+        /// <summary>
+        /// AddKey, adds a key to the key chain
+        /// </summary>
+        /// <param name="aKey"></param>
+        public void AddKey(IKey aKey) { KeyStore.Add(aKey); }
+
+        /// <summary>
+        /// RemoveKey, removes a key from the key chain.
+        /// </summary>
+        /// <param name="aKey"></param>
+        public void RemoveKey(IKey aKey) { KeyStore.Remove(aKey); }
+
+        /// <summary>
+        /// HasKey, determines if the key is contained in the key chain.
+        /// </summary>
+        /// <param name="aKey"></param>
+        /// <returns></returns>
+        public bool HasKey(IKey aKey)
+        {
+            return KeyStore.FirstOrDefault<IKey>(
+                k => k.KeyGuid.Equals(aKey.KeyGuid)) == null;
+        }
+
+        /// <summary>
+        /// <c>CanUnLock</c>, determines if this KeyChain <c>CanUnLock</c> whatever 
+        /// <see cref="IHasLock"/> has been passed.
+        /// </summary>
+        /// <param name="aHasLock">Anything that has a lock.</param>
+        /// <returns><value>true - if the *thing* that has a lock can be unlocked.</value></returns>
+        public bool CanUnLock(IHasLock aHasLock) { return HasKey(aHasLock.KeyGuid); }
+
+        /// <summary>
+        /// A keychain is a collection of Guids
+        /// </summary>
+        public List<IKey> KeyStore { get; set; }
+    }
+}

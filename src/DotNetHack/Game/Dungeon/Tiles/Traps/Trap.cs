4bf3c67a9e0463ef532e74295534c5db9756c028
Pete Jensen <jensen.petej@gmail.com>
2011-07-23 19:53:37 -0700

    Added Trap, TrapFallingRock, TrapSpikePit.

---
+++ src/DotNetHack/Game/Dungeon/Tiles/Traps/Trap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotNetHack.Game.Interfaces;
+
+namespace DotNetHack.Game.Dungeon.Tiles.Traps
+{
+    /// <summary>
+    /// Trap
+    /// </summary>
+    public abstract class Trap : Tile, IGlyph
+    {
+        /// <summary>
+        /// Creates a new instance of Trap
+        /// <remarks>This is the most basic form of trap.</remarks>
+        /// </summary>
+        public Trap()
+            : base('^', Colour.Standard)
+        { }
+
+        /// <summary>
+        /// TrapEventArgs
+        /// </summary>
+        public class TrapEventArgs : EventArgs
+        {
+            /// <summary>
+            /// Creates a new instance of trap event args
+            /// <param name="aActor">The actor that triggered the trap</param>
+            /// </summary>
+            public TrapEventArgs(Actor aActor) { TrapTarget = aActor; }
+
+            /// <summary>
+            /// TrapTarget, the actor responsibile for triggering the trap.
+            /// </summary>
+            public Actor TrapTarget { get; set; }
+        }
+
+        /// <summary>
+        /// Occurs when the trap is triggered.
+        /// </summary>
+        /// <param name="e"></param>
+        protected virtual void OnTrapTriggeredEvent(TrapEventArgs e) 
+        {
+            if (TriggerEvent != null)
+                TriggerEvent(this, e);
+        }
+
+        /// <summary>
+        /// The trap trigger event handler.
+        /// </summary>
+        public event EventHandler<TrapEventArgs> TriggerEvent;
+    }
+}
---
+++ src/DotNetHack/Game/Dungeon/Tiles/Traps/TrapSpikePit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetHack.Game.Dungeon.Tiles.Traps
+{
+    /// <summary>
+    /// TrapSpikePit
+    /// </summary>
+    public class TrapSpikePit : Trap
+    {
+        /// <summary>
+        /// Creates a new instance of a spike pit trap.
+        /// </summary>
+        public TrapSpikePit()
+            : base() 
+        {
+            TriggerEvent += new EventHandler<TrapEventArgs>(TrapSpikePit_TriggerEvent);
+        }
+
+        /// <summary>
+        /// Occurs when the spike pit trap is triggered.
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">Trap event arguments.</param>
+        void TrapSpikePit_TriggerEvent(object sender, Trap.TrapEventArgs e)
+        {
+
+        }
+    }
+}

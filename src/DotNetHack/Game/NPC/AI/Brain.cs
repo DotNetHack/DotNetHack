10f97504bb919372e511ca82bd95aea072c2ee92
Pete Jensen <jensen.petej@gmail.com>
2011-07-30 13:38:40 -0700

    Adding Brain and FSM. FSM is genericized.

--- src/DotNetHack/Game/NPC/AI/Brain.cs
+++ src/DotNetHack/Game/NPC/AI/Brain.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DotNetHack.Game.Dungeon;
 using DotNetHack.Utility.Graph.Algorithm;
+using DotNetHack.Game.Events;
 
 namespace DotNetHack.Game.NPC.AI
 {
@@ -12,11 +14,68 @@ namespace DotNetHack.Game.NPC.AI
     public class Brain
     {
         /// <summary>
+        /// Occures when this brain becomes aware of the player.
+        /// </summary>
+        public event EventHandler<ActorEventArgs> OnSeePlayer;
+
+        /// <summary>
+        /// Occurs when this brain becomes aware of another monster.
+        /// </summary>
+        public event EventHandler<ActorEventArgs> OnSeeMonster;
+
+        /// <summary>
+        /// Occurs when this brain sees an item.
+        /// </summary>
+        public event EventHandler<GameEventArgs> OnSeeItem;
+
+        /// <summary>
         /// Supports serialization.
         /// </summary>
-        public Brain() 
-        {
+        public Brain() { }
 
+        /// <summary>
+        /// Creates a new instance of Brain.  A brain requires a reference to the
+        /// actor and the surroundings or dungeon.
+        /// </summary>
+        /// <param name="aSelf">The actor that this brain belongs to.</param>
+        /// <param name="aDungeon">The surroundings the brain is aware of.</param>
+        public Brain(Actor aSelf, Dungeon.Dungeon3 aDungeon) 
+        {
+            Self = aSelf;
+            Dungeon = aDungeon;
+            PathFinding = new DungeonPathFinding(Dungeon);
+        }
+
+        /// <summary>
+        /// The brain has "awareness" of it self.
+        /// </summary>
+        Actor Self { get; set; }
+
+        /// <summary>
+        /// The brain has "awareness" of it's surroundings.
+        /// </summary>
+        Dungeon3 Dungeon { get; set; }
+
+        /// <summary>
+        /// The brains ability to find a way to things.
+        /// </summary>
+        DungeonPathFinding PathFinding { get; set; }
+
+        /// <summary>
+        /// SwitchDungeon allows for changing the dungeon.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">thrown when dungeon is null.</exception>
+        public void SwitchDungeon(Dungeon.Dungeon3 aDungeon)
+        {
+            // Make sure the passed parameter is not null.
+            if (aDungeon == null)
+                throw new ArgumentNullException("Cannot switch to a null dungeon.");
+
+            // Set the dungeon.
+            Dungeon = aDungeon;
+
+            // Update path finding.
+            PathFinding = new DungeonPathFinding(Dungeon);
         }
     }
 }
---
+++ src/DotNetHack/Game/NPC/AI/FSM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetHack.Game.NPC.AI
+{
+    /// <summary>
+    /// A genericized finite state machine.
+    /// </summary>
+    /// <typeparam name="S">The state type</typeparam>
+    /// <typeparam name="H">The heuristic, a function of H, returning S.</typeparam>
+    /// <typeparam name="G">Other</typeparam>
+    public class FSM<S, H, G> where H : Func<H, S>
+    {
+        /// <summary>
+        /// Creates a new instance of the finite state machine.
+        /// </summary>
+        public FSM(int s)
+        {
+            Heuristics = new H[s];
+        }
+
+        /// <summary>
+        /// An array of heuristics.
+        /// </summary>
+        H[] Heuristics { get; set; }
+    }
+}

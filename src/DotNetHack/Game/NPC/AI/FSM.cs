2d8e7112c0c401fcd6a352a1e95770d16cb4a07e
Pete Jensen <jensen.petej@gmail.com>
2011-07-30 16:08:08 -0700

    refined state machine.

--- src/DotNetHack/Game/NPC/AI/FSM.cs
+++ src/DotNetHack/Game/NPC/AI/FSM.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/// FSM can be used for quick genericized state machines.
+/// Copyright (C) 2011  Peter Jensen
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,23 +23,45 @@ namespace DotNetHack.Game.NPC.AI
 {
     /// <summary>
     /// A genericized finite state machine.
+    /// <typeparam name="S">The type of state this FSM may be in.</typeparam>
     /// </summary>
-    /// <typeparam name="S">The state type</typeparam>
-    /// <typeparam name="H">The heuristic, a function of H, returning S.</typeparam>
-    /// <typeparam name="G">Other</typeparam>
-    public class FSM<S, H, G> where H : Func<H, S>
+    public class FSM<S>
     {
         /// <summary>
         /// Creates a new instance of the finite state machine.
+        /// <param name="initialState">The initial state of the state machine.</param>
         /// </summary>
-        public FSM(int s)
+        public FSM(S aInitialState) { CurrentState = aInitialState; }
+
+        /// <summary>
+        /// Creates a new instance of the finite state machine.
+        /// <remarks>The default constructor will default the state in the 
+        /// standard manner using <code>default(S)</code></remarks>
+        /// </summary>
+        public FSM() { CurrentState = default(S); }
+
+        /// <summary>
+        /// The current state of the state machine.
+        /// <remarks>readonly</remarks>
+        /// </summary>
+        public S CurrentState { get; private set; }
+
+        /// <summary>
+        /// Reads and yield returns the current state then moves
+        /// to the next state.
+        /// </summary>
+        public IEnumerable<S> Next
         {
-            Heuristics = new H[s];
+            get
+            {
+                yield return CurrentState;
+                CurrentState = fOf(CurrentState);
+            }
         }
 
         /// <summary>
-        /// An array of heuristics.
+        /// The transition function / table.
         /// </summary>
-        H[] Heuristics { get; set; }
+        Func<S, S> fOf { get; set; }
     }
 }

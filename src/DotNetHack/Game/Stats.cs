b8b04dac6cd3b2f9b4edbffd85730d3e228878cb
Pete Jensen <jensen.petej@gmail.com>
2011-09-17 16:58:21 -0700

    Added a StatsBase object as is required before various effects, potions, etc can be fully implemented.  This object supports addition, subtraction and unary-negate.

--- src/DotNetHack/Game/Stats.cs
+++ src/DotNetHack/Game/Stats.cs
@@ -6,47 +6,171 @@ using System.Xml.Serialization;
 
 namespace DotNetHack.Game
 {
+    /// <summary>
+    /// StatsBase is a simple stats abstraction.
+    /// items, players, npcs, enchantments may use this at will.
+    /// </summary>
     [Serializable]
-    public class Stats
+    public class StatsBase
     {
-        public Stats() { }
+        /// <summary>
+        /// create a new instance of stats base.
+        /// </summary>
+        public StatsBase() { }
 
+        /// <summary>
+        /// the level
+        /// </summary>
         [XmlAttribute]
         public int Level { get; set; }
 
+        /// <summary>
+        /// speed
+        /// </summary>
         [XmlAttribute]
         public int Speed { get; set; }
 
+        /// <summary>
+        /// AC or armour class
+        /// </summary>
         [XmlAttribute]
         public int ArmourClass { get; set; }
 
+        /// <summary>
+        /// strength
+        /// </summary>
         [XmlAttribute]
         public int Strength { get; set; }
 
+        /// <summary>
+        /// endurance
+        /// </summary>
         [XmlAttribute]
         public int Endurance { get; set; }
 
+        /// <summary>
+        /// agility
+        /// </summary>
         [XmlAttribute]
         public int Agility { get; set; }
 
+        /// <summary>
+        /// wisdom.
+        /// </summary>
         [XmlAttribute]
         public int Wisdom { get; set; }
 
+        /// <summary>
+        /// intelligence.
+        /// </summary>
         [XmlAttribute]
         public int Intelligence { get; set; }
 
+        /// <summary>
+        /// charisma
+        /// </summary>
         [XmlAttribute]
         public int Charisma { get; set; }
 
+        /// <summary>
+        /// experience.
+        /// </summary>
         [XmlAttribute]
         public int Experience { get; set; }
 
+        /// <summary>
+        /// luck
+        /// </summary>
         [XmlAttribute]
         public int Luck { get; set; }
 
+        /// <summary>
+        /// perception.
+        /// </summary>
         [XmlAttribute]
         public int Perception { get; set; }
 
+        /// <summary>
+        /// plus operator for <see cref="StatsBase"/>.
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns>The result of adding the two <c>StatsBase</c> 
+        /// objects.</returns>
+        public static StatsBase operator +(StatsBase a, StatsBase b)
+        {
+            return new StatsBase()
+            {
+                Strength = a.Speed + b.Speed,
+                Perception = a.Perception + b.Perception,
+                Endurance = a.Endurance + b.Endurance,
+                Charisma = a.Charisma + b.Charisma,
+                Intelligence = a.Intelligence + b.Intelligence,
+                Agility = a.Agility + b.Agility,
+                Luck = a.Luck + b.Luck,
+                ArmourClass = a.ArmourClass + b.ArmourClass,
+                Experience = a.Experience + b.Experience,
+                Level = a.Level + b.Level,
+                Speed = a.Speed + b.Speed,
+                Wisdom = a.Wisdom + b.Wisdom,
+            };
+        }
+
+        /// <summary>
+        /// minus operator for <see cref="StatsBase"/>.
+        /// See: unary -
+        /// </summary>
+        /// <param name="a">LHS</param>
+        /// <param name="b">RHS</param>
+        /// <returns>The result of subtracting the two <c>StatsBase</c> 
+        /// objects.</returns>
+        public static StatsBase operator -(StatsBase a, StatsBase b)
+        {
+            return a + (-b);
+        }
+
+        /// <summary>
+        /// unary (negate) for a <see cref="StatsBase"/> object.
+        /// <remarks>leveraged to perform subtraction.</remarks>
+        /// </summary>
+        /// <param name="a">The StatsBase object to negate.</param>
+        /// <returns>A statsbase object with each of the stats negated.</returns>
+        public static StatsBase operator -(StatsBase a)
+        {
+            return new StatsBase()
+            {
+                Agility = -a.Agility,
+                ArmourClass = -a.ArmourClass,
+                Charisma = -a.Charisma,
+                Endurance = -a.Endurance,
+                Experience = -a.Experience,
+                Intelligence = -a.Intelligence,
+                Level = -a.Level,
+                Luck = -a.Luck,
+                Perception = -a.Perception,
+                Speed = -a.Speed,
+                Strength = -a.Strength,
+                Wisdom = -a.Wisdom,
+            };
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public class Stats : StatsBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Stats()
+            : base()
+        { }
+
+        /// <summary>
+        /// Health is a derrived stat.
+        /// </summary>
         [XmlIgnore]
         public int Health
         {
@@ -67,11 +191,8 @@ namespace DotNetHack.Game
                 // health ceiling
                 if (_health >= HitPoints)
                     _health = HitPoints;
-
             }
         }
-
-        private int _health;
 
         /// <summary>
         /// Derrived attribute
@@ -89,11 +210,18 @@ namespace DotNetHack.Game
         /// <summary>
         /// Derrived attribute
         /// </summary>
+        [XmlIgnore]
         public int ManaPoints { get { return (int)(2.5 * Intelligence); } }
 
         /// <summary>
         /// Triggered on any change in health.
+        /// Note: Should be elsewhere.
         /// </summary>
         public event EventHandler OnHealthChanged;
+
+        /// <summary>
+        /// Health backing store.
+        /// </summary>
+        private int _health;
     }
 }

1323c06e611145bc23a9ab32320a63ab704587a4
Pete Jensen <jensen.petej@gmail.com>
2011-09-24 19:46:09 -0700

    Added FoodBase.

--- src/DotNetHack/Game/Interfaces/IFood.cs
+++ src/DotNetHack/Game/Interfaces/IFood.cs
@@ -6,6 +6,19 @@ namespace DotNetHack.Game.Interfaces
 	/// </summary>
 	public interface IFood : IConsumable
 	{
+        /// <summary>
+        /// The number of calories of this food-stuff.
+        /// </summary>
+        int Calories { get; set; }
+
+        /// <summary>
+        /// If this food tainted
+        /// </summary>
+        bool Tainted { get; set; }
+
+        /// <summary>
+        /// Eat this food
+        /// </summary>
         void Eat();
 	}
 }

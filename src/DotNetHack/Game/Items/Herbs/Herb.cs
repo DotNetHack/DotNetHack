25a9701c58492be8f21665cda76d36b947bb2d7d
Pete Jensen <jensen.petej@gmail.com>
2011-09-25 12:09:48 -0700

    Added Herb item

---
+++ src/DotNetHack/Game/Items/Herbs/Herb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetHack.Game.Items.Herbs
+{
+    /// <summary>
+    /// Herb
+    /// </summary>
+    [Serializable]
+    public class Herb : Item
+    {
+        /// <summary>
+        /// Supports serialization.
+        /// </summary>
+        public Herb() { }
+
+        /// <summary>
+        /// Creates a new instance of Herb
+        /// </summary>
+        public Herb(string aName)
+            : base(aName, '"', Colour.Grass)
+        { }
+    }
+}

8021c25e9c24cd5aa671f4ad67601124fc3f9544
Pete Jensen <jensen.petej@gmail.com>
2011-09-25 12:10:10 -0700

    Created ISpawn interface.

---
+++ src/DotNetHack/Game/Interfaces/ISpawn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetHack.Game.Interfaces
+{
+    /// <summary>
+    /// ISpawn
+    /// </summary>
+    public interface ISpawn<T> where T : IItem
+    {
+        /// <summary>
+        /// The last time this spawn was visited.
+        /// </summary>
+        DateTime LastVisited { get; set; }
+
+        /// <summary>
+        /// The resource that is spawned by this spawn.
+        /// </summary>
+        T Resource { get; set; }
+    }
+}

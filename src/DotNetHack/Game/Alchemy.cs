8d958d2883a8673dfe2864b1745c2058c03b07cf
Pete Jensen <jensen.petej@gmail.com>
2011-10-02 19:21:22 -0700

    Created Alchemy.cs

---
+++ src/DotNetHack/Game/Alchemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotNetHack.Game.Interfaces;
+using DotNetHack.Game.Items.Herbs;
+using DotNetHack.Game.Items.Equipment.Tools;
+
+namespace DotNetHack.Game
+{
+    public class Alchemy
+    {
+        public static IPotion Combine(MortarAndPestle aMortarAndPestle,
+            Herb[] aHerbs) 
+        {
+            
+            return null;
+        } 
+    }
+}

239c4b62bffe632ac1b509d8ed75fe12287430d5
Pete Jensen <jensen.petej@gmail.com>
2011-10-18 20:16:38 -0700

    quest completion crit?

--- src/DotNetHack/Game/Quests/Quest.cs
+++ src/DotNetHack/Game/Quests/Quest.cs
@@ -11,17 +11,53 @@ namespace DotNetHack.Game.Quests
     /// </summary>
     public class Quest
     {
-        public Quest() 
+        /// <summary>
+        /// 
+        /// </summary>
+        public Quest(Func<Player, bool> c)
         {
+            CompletionCriteria = c;
         }
 
-        public Quest[] SubQuests { get; set; }
+        /// <summary>
+        /// all subquests
+        /// </summary>
+        public LinkedList<Quest> SubQuests { get; set; }
 
+        /// <summary>
+        /// the amount of experience awarded for completing this quest
+        /// </summary>
         public int Experience { get; set; }
 
+        /// <summary>
+        /// the name of this quest
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// the quest's description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// CompletionCriteria
+        /// </summary>
+        public readonly Func<Player, bool> CompletionCriteria { get; set; }
+
+        /// <summary>
+        /// QuestEvent
+        /// </summary>
         public class QuestEvent : GameEventArgs
         {
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="aQuest"></param>
             public QuestEvent(Quest aQuest) { Quest = aQuest; }
+
+            /// <summary>
+            /// 
+            /// </summary>
             public Quest Quest { get; set; }
         }
     }

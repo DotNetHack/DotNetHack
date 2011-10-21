74cf26288dc86f2ef7c8d0057885e614b1811210
Pete Jensen <jensen.petej@gmail.com>
2011-10-21 15:50:03 -0700

    Added Alembic and Calcinator

---
+++ src/DotNetHack/Game/Items/Equipment/Tools/Calcinator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetHack.Game.Items.Equipment.Tools
+{
+    public class Calcinator : AlchemyEquipment
+    {
+        public Calcinator(double m)
+            : base("Calcinator", new Glyph('π', Colour.Standard), 0.0, 0.0, 0.0, -m)
+        { }
+    }
+}

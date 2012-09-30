c1b4c0844d632d864966e6f4f65a8adb33acc4e0
Pete Jensen <jensen.petej@gmail.com>
2012-09-29 23:11:27 -0400

    Added DNHException
    - Core exception class
    - Map & Tile (stubs)

---
+++ src/DotNetHack.Core/Exceptions/DNHException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetHack.Core.Exceptions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class DNHException : Exception
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public DNHException() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        public DNHException(string message)
+        {
+        }
+
+        /// <summary>
+        /// DNHException
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
+        public DNHException(string message, Exception inner)
+        {
+ 
+        }
+    }
+}
---
+++ src/DotNetHack.Core/Map.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetHack.Core
+{
+    [Serializable]
+    public class Map
+    {
+        /// <summary>
+        /// Map
+        /// </summary>
+        /// <param name="width">width of the map</param>
+        /// <param name="height">height of the map</param>
+        /// <param name="depth">the depth of the map</param>
+        public Map(int width, int height, int depth)
+        {
+            Tiles = new Tile[width, height, depth];
+        }
+
+        /// <summary>
+        /// Tiles
+        /// </summary>
+        Tile[, ,] Tiles { get; set; }
+    }
+}

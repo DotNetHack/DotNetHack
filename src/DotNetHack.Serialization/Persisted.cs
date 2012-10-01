1f43824d68e93d9b8165a593a2ff3f34e4fff1f8
Pete Jensen <jensen.petej@gmail.com>
2012-10-01 18:57:40 -0400

    Updated DotNetHack.Serialization

--- src/DotNetHack.Serialization/Persisted.cs
+++ src/DotNetHack.Serialization/Persisted.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace DotNetHack.Serialization
 {

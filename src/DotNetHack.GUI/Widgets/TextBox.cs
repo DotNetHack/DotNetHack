bb319040614da400fa1dfc07989c3afe86268446
Pete Jensen <jensen.petej@gmail.com>
2013-03-16 18:24:10 -0400

    Added new widget called "TextBox"

---
+++ src/DotNetHack.GUI/Widgets/TextBox.cs
@@ -0,0 +1,92 @@
+﻿using DotNetHack.GUI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetHack.GUI.Widgets
+{
+    /// <summary>
+    /// TextBox
+    /// </summary>
+    public class TextBox : Widget
+    {
+        /// <summary>
+        /// TextBox
+        /// </summary>
+        /// <param name="label"></param>
+        /// <param name="inputMaxLength"></param>
+        public TextBox(int x, int y, int inputMaxLength) 
+            : base("", x, y, inputMaxLength, 1)
+        {
+            KeyboardEvent += TextBox_KeyboardEvent;
+        }
+
+        /// <summary>
+        /// TextBox_KeyboardEvent
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void TextBox_KeyboardEvent(object sender, Events.GUIKeyboardEventArgs e)
+        {
+            if (!Visible)
+                return;
+
+            char tmpChar = e.ConsoleKeyInfo.KeyChar;
+
+            switch (e.ConsoleKeyInfo.Key)
+            {
+                default:
+                    if (!char.IsLetterOrDigit(tmpChar))
+                        return;
+
+                    AddNewChar(tmpChar);
+                    break;
+
+                case ConsoleKey.Backspace:
+                    EraseLastChar();
+                    break;
+
+                case ConsoleKey.Enter:
+                    OnEntered(Text);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// EraseChar
+        /// </summary>
+        void EraseLastChar()
+        {
+            if (Text.Length <= 0)
+                return;
+
+            Text = Text.Substring(0, Text.Length - 1);
+        }
+
+        /// <summary>
+        /// AddChar
+        /// </summary>
+        /// <param name="ch"></param>
+        void AddNewChar(char ch)
+        {
+            if (Text.Length + 1 > MaxLength)
+                return;
+
+            Text += ch;
+
+            Console.Invalidate();
+        }
+
+        /// <summary>
+        /// OnEnter
+        /// </summary>
+        public Action<string> OnEntered { get; protected set; }
+
+        /// <summary>
+        /// MaxLength
+        /// </summary>
+        public int MaxLength { get; protected set; }
+    }
+}

5d8cf9f4bc4db138ce7d4d31f5c53bbe50e1ea23
Pete Jensen <jensen.petej@gmail.com>
2013-03-18 23:54:28 -0400

    Added auth service method

--- wire/DNHPacket.thrift
+++ wire/DNHPacket.thrift
@@ -4,6 +4,7 @@ struct DNHPacket {
 }
 
 service DNHService {
+	i32 authenticate(1: string userName, 2: string password),
 	void sendPacket(1: DNHPacket packet),
 	DNHPacket retrievePacket(1: i32 uid)
 }
